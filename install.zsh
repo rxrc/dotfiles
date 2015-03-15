3f1cd8c7812bd03c9da7bb6860eda29cb8a1cdaa
Evan Sosenko <razorx@evansosenko.com>
2015-03-15 11:21:54 -0700

    Echo commands in install.zsh.

--- install.zsh
+++ install.zsh
@@ -1,7 +1,12 @@
 #!/usr/bin/env zsh
 
+echo '$ ./bootstrap.zsh'
 ./bootstrap.zsh && \
-bower update    && \
-bundle update   && \
-curate -v       && \
-./update.zsh
+  echo '\n$ bower update\n'  && \
+  bower update  && \
+  echo '\n$ bundle update\n' && \
+  bundle update && \
+  echo '\n$ curate -v\n'     && \
+  curate -v     && \
+  echo '\n$ ./update.zsh'  && \
+  ./update.zsh
