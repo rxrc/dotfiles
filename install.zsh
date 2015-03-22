cefa3ce64a98c3824990e7d39f15d720137faa7d
Evan Sosenko <razorx@evansosenko.com>
2015-03-22 15:55:41 -0700

    Use set -e.

--- install.zsh
+++ install.zsh
@@ -1,12 +1,18 @@
 #!/usr/bin/env zsh
 
+set -e
+
 echo '$ ./bootstrap.zsh'
-./bootstrap.zsh && \
-  echo '\n$ bower update\n'  && \
-  bower update  && \
-  echo '\n$ bundle update\n' && \
-  bundle update && \
-  echo '\n$ curate -v\n'     && \
-  curate -v     && \
-  echo '\n$ ./update.zsh'  && \
-  ./update.zsh
+./bootstrap.zsh
+
+echo '\n$ bower update\n'
+bower update
+
+echo '\n$ bundle update\n'
+bundle update
+
+echo '\n$ curate -v\n'
+curate -v
+
+echo '\n$ ./update.zsh'
+./update.zsh
