fcc9a180d07aee4092e9abe3e7805b5c2aa36922
Evan Sosenko <razorx@evansosenko.com>
2015-04-11 17:51:15 -0700

    Remove environment from units.zsh.

--- units.zsh
+++ units.zsh
@@ -2,7 +2,6 @@
 
 enabled=()
 disabled=()
-environment=()
 
 for unit in $enabled; do
   echo "Enable: $unit"
@@ -12,11 +11,6 @@ done
 for unit in $disabled; do
   echo "Disable: $unit"
   sudo systemctl --user disable $unit
-done
-
-for unit in $environment; do
-  echo "Set: $unit"
-  systemctl --user set-environment $unit
 done
 
 exit 0
