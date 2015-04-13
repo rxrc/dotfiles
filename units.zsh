0e44fc6c7834907537e3cfab979838a479c1f2fc
Evan Sosenko <razorx@evansosenko.com>
2015-04-12 21:03:23 -0700

    Change echos in units.zsh.

--- units.zsh
+++ units.zsh
@@ -8,12 +8,12 @@ enabled+=( 'keymap.service' )
 enabled+=( 'awesome.service' )
 
 for unit in $enabled; do
-  echo "Enable: $unit"
+  echo "[Enable] $unit"
   systemctl --user enable $unit
 done
 
 for unit in $disabled; do
-  echo "Disable: $unit"
+  echo "[Disable] $unit"
   sudo systemctl --user disable $unit
 done
 
