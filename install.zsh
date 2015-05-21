43002083423c70c3159c52d367c0056fefba1605
Evan Sosenko <razorx@evansosenko.com>
2015-05-20 18:10:35 -0700

    Create npmrc in bootstrap.zsh.

--- install.zsh
+++ install.zsh
@@ -8,12 +8,12 @@ echo '$ ./bootstrap.zsh'
 echo '\n$ pip install --user --upgrade -r requirements.txt'
 pip install --user --upgrade -r requirements.txt
 
-cp dotfiles/npmrc $HOME/.npmrc
 echo '\n$ ./npm_modules.zsh'
 ./npm_modules.zsh
 
 echo '\n$ bower update\n'
-$HOME/.npm/bin/bower update
+command -v bower && bower update
+command -v bower || $HOME/.npm/bin/bower update
 
 echo '\n$ bundle update\n'
 bundle update
