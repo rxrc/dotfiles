41c75ad9fd4fb2143b480b4373ce380e5789821a
Evan Sosenko <razorx@evansosenko.com>
2015-06-30 16:02:11 -0700

    Add node_name to knife.rb.

--- dotfiles/chef/knife.rb
+++ dotfiles/chef/knife.rb
@@ -2,5 +2,6 @@ home_dir = Dir.home
 chef_dir = File.join(home_dir, '.chef')
 username = ( ENV['USER'] || ENV['USERNAME'] ).downcase
 
+node_name     username
 client_key    File.join(chef_dir, "#{username}.pem")
 cookbook_path File.join(home_dir, 'build', 'cookbooks')
