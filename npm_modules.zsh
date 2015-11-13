25339da8093547a9863ba4974198699f26b926e9
Evan Sosenko <razorx@evansosenko.com>
2015-11-12 17:51:35 -0800

    Use ember-cli@^1.13.12

--- npm_modules.zsh
+++ npm_modules.zsh
@@ -8,7 +8,7 @@ npm_modules=()
 command -v npm >/dev/null 2>&1 || exit 0
 
 npm_modules+=('bower@^1.6.4')
-npm_modules+=('ember-cli@1.13.12')
+npm_modules+=('ember-cli@^1.13.12')
 npm_modules+=('eslint_d@^2.3.1')
 
 for module in $npm_modules; do
