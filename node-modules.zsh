b934772e2cef2e1f7fc181b2b6322fafaabed21b
Evan Sosenko <razorx@evansosenko.com>
2015-12-06 19:40:50 -0800

    Add grunt and gulp cli tools

--- node-modules.zsh
+++ node-modules.zsh
@@ -9,6 +9,8 @@ command -v npm >/dev/null 2>&1 || exit 0
 
 node_modules+=('bower@^1.6.4')
 node_modules+=('ember-cli@^1.13.12')
+node_modules+=('grunt-cli@^0.1.13')
+node_modules+=('gulp@^3.9.0')
 node_modules+=('standard@^5.3.1')
 
 npm install --global ${node_modules}
