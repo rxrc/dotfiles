1b7053b49c4554f27b0342c17adc8339ddcfcf15
Evan Sosenko <razorx@evansosenko.com>
2015-12-07 22:38:33 -0800

    Update all lein plugins

--- dotfiles/lein/profiles.clj
+++ dotfiles/lein/profiles.clj
@@ -1,3 +1,3 @@
-{:user {:plugins [[jonase/eastwood "0.2.1"]
-                  [cljfmt "0.1.10"]
-                  [cider/cider-nrepl "0.8.2"]]}}
+{:user {:plugins [[jonase/eastwood "0.2.2"]
+                  [cljfmt "0.3.0"]
+                  [cider/cider-nrepl "0.10.0"]]}}
