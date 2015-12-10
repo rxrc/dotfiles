320fd7e90ef83363e50e2574ddc5686dcfbfd737
Evan Sosenko <razorx@evansosenko.com>
2015-12-09 21:08:45 -0800

    Add lein-test-refresh

--- dotfiles/lein/profiles.clj
+++ dotfiles/lein/profiles.clj
@@ -5,6 +5,7 @@
       [lein-cljfmt "0.3.0"]
       [lein-kibit "0.1.2"]
       [lein-pprint "1.1.2"]
+      [com.jakemccrary/lein-test-refresh "0.11.0"]
       [cider/cider-nrepl "0.10.0"]
       [jonase/eastwood "0.2.2"]
       [venantius/ultra "0.4.0"]
