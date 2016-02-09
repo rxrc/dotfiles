dee57afdd1724e6bab75c9860e8ac3c88e66d931
Evan Sosenko <razorx@evansosenko.com>
2016-02-09 11:08:53 -0800

    Add coffeelint

--- node-modules.zsh
+++ node-modules.zsh
@@ -9,6 +9,7 @@ command -v npm >/dev/null 2>&1 || exit 0
 
 node_modules+=('bower@^1.6.4')
 node_modules+=('brunch@^2.2.3')
+node_modules+=('coffeelint@^1.14.2')
 node_modules+=('ember-cli@^1.13.12')
 node_modules+=('grunt-cli@^0.1.13')
 node_modules+=('gulp@^3.9.0')
