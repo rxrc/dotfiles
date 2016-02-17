a9fa36cdf6df85fcd91da823f0406235dab3e051
Evan Sosenko <razorx@evansosenko.com>
2016-02-16 19:32:27 -0800

    Update knife.rb

--- dotfiles/chef/knife.rb
+++ dotfiles/chef/knife.rb
@@ -1,7 +1,8 @@
-home_dir = Dir.home
-chef_dir = File.join(home_dir, '.chef')
-username = ( ENV['USER'] || ENV['USERNAME'] ).downcase
+home_dir   = Dir.home
+chef_dir   = File.join(home_dir, '.chef')
+secret_dir = File.join(chef_dir, 'private')
+username   = ( ENV['USER'] || ENV['USERNAME'] ).downcase
 
 node_name     username
-client_key    File.join(chef_dir, "#{username}.pem")
+client_key    File.join(secret_dir, "#{username}.pem")
 cookbook_path File.join(home_dir, 'build', 'cookbooks')
