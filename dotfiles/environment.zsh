737215432d133d10cb2ea76da7bdb532da659745
Evan Sosenko <razorx@evansosenko.com>
2016-06-08 12:15:21 -0700

    Add .environment.zsh with Base16 theme

---
+++ dotfiles/environment.zsh
@@ -0,0 +1,7 @@
+# Set Base16 Shell theme.
+export BASE16_THEME=default
+export BASE16_TYPE=dark
+
+# Set Vim colors explicitly.
+export VIM_COLOR=base16-$BASE16_THEME
+export VIM_BACKGROUND=$BASE16_TYPE
