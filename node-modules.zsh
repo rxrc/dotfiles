c6ecff4b135a1519cce2d69450ed633fad8649af
Evan Sosenko <razorx@evansosenko.com>
2016-06-17 09:49:47 -0700

    Update all node modules

--- node-modules.zsh
+++ node-modules.zsh
@@ -7,16 +7,16 @@ node_modules=()
 
 command -v npm >/dev/null 2>&1 || exit 0
 
-node_modules+=('babel-eslint@^6.0.2')
-node_modules+=('bower@^1.6.4')
-node_modules+=('brunch@^2.2.3')
-node_modules+=('coffeelint@^1.14.2')
-node_modules+=('ember-cli@^2.3.0')
+node_modules+=('babel-eslint@^6.0.4')
+node_modules+=('bower@^1.7.9')
+node_modules+=('brunch@^2.8.2')
+node_modules+=('coffeelint@^1.15.7')
+node_modules+=('ember-cli@^2.6.1')
 node_modules+=('grunt-cli@^1.2.0')
-node_modules+=('gulp@^3.9.0')
-node_modules+=('sass-lint@^1.5.1')
-node_modules+=('standard@^6.0.4')
-node_modules+=('standard-format@^2.1.0')
+node_modules+=('gulp-cli@^1.2.1')
+node_modules+=('sass-lint@^1.8.0')
+node_modules+=('standard@^7.1.2')
+node_modules+=('standard-format@^2.2.1')
 
 npm install --global ${node_modules}
 
