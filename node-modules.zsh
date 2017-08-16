9f68ea465ae58b4a27638bc097a1d9ed9478c07e
Evan Sosenko <razorx@evansosenko.com>
2017-08-16 11:39:54 -0700

    Use awesome-copycats v4.3.3

--- node-modules.zsh
+++ node-modules.zsh
@@ -7,12 +7,12 @@ node_modules=()
 
 command -v npm >/dev/null 2>&1 || exit 0
 
-node_modules+=('bower@^1.7.9')
+node_modules+=('bower@^1.8.0')
 node_modules+=('brunch@^2.8.2')
 node_modules+=('ember-cli@^2.7.0')
 node_modules+=('grunt-cli@^1.2.0')
 node_modules+=('gulp-cli@^1.2.2')
-node_modules+=('tern@^0.20.0')
+node_modules+=('tern@^0.21.0')
 node_modules+=('jsctags@^5.0.1')
 
 npm install --global ${node_modules}
