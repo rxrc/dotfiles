fb07523c92ee78b7da51bc2006b29bcacc82a524
Evan Sosenko <razorx@evansosenko.com>
2017-11-01 11:34:20 -0700

    Add json-diff and gh-home

--- node-modules.zsh
+++ node-modules.zsh
@@ -14,6 +14,8 @@ node_modules+=('grunt-cli@^1.2.0')
 node_modules+=('gulp-cli@^1.2.2')
 node_modules+=('tern@^0.21.0')
 node_modules+=('jsctags@^5.0.1')
+node_modules+=('json-diff@^0.5.2')
+node_modules+=('gh-home@^1.4.1')
 
 npm install --global ${node_modules}
 
