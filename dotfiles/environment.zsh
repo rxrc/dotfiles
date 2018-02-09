861e2191a17aa5d4a1b9bb36760798e3ac44e6bc
Evan Sosenko <razorx@evansosenko.com>
2018-02-09 09:38:51 -0800

    Remove environment.zsh file

--- dotfiles/environment.zsh
+++
@@ -1,7 +0,0 @@
-# Set Base16 Shell theme.
-export BASE16_THEME=tomorrow
-export BASE16_TYPE=night
-
-# Set Vim colors explicitly.
-export VIM_COLOR=base16-$BASE16_THEME
-export VIM_BACKGROUND=$BASE16_TYPE
