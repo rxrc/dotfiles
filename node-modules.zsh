21ac8f2e49e137a49b1fb4255e9c34e0d29fa2b0
Evan Sosenko <razorx@evansosenko.com>
2018-05-14 13:36:10 -0700

    Remove some node CLI tools

--- node-modules.zsh
+++ node-modules.zsh
@@ -10,11 +10,7 @@ command -v npm >/dev/null 2>&1 || exit 0
 npm config set sign-git-tag true
 
 node_modules+=('bower@^1.8.0')
-node_modules+=('brunch@^2.8.2')
-node_modules+=('ember-cli@^2.7.0')
 node_modules+=('gh-home@^1.4.1')
-node_modules+=('grunt-cli@^1.2.0')
-node_modules+=('gulp-cli@^1.2.2')
 node_modules+=('jsctags@^5.0.1')
 node_modules+=('json-diff@^0.5.2')
 node_modules+=('tern@^0.21.0')
