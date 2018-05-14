a384497cdf8f5a5992bcf47427505bbb7016f3cd
Evan Sosenko <razorx@evansosenko.com>
2018-05-13 22:52:40 -0700

    Update lein profile

--- dotfiles/lein/profiles.clj
+++ dotfiles/lein/profiles.clj
@@ -1,22 +1,23 @@
 {
   :user {
     :plugins [
-      [lein-bikeshed "0.2.0"]
-      [lein-cljfmt "0.3.0"]
-      [lein-gen "0.2.1"]
-      [lein-kibit "0.1.2"]
-      [lein-pprint "1.1.2"]
-      [com.jakemccrary/lein-test-refresh "0.11.0"]
-      [cider/cider-nrepl "0.10.0"]
-      [jonase/eastwood "0.2.2"]
-      [venantius/ultra "0.4.0"]
+      [lein-ancient "0.6.15"]
+      [lein-bikeshed "0.5.1"]
+      [lein-cljfmt "0.5.7"]
+      [lein-gen "0.2.2"]
+      [lein-kibit "0.1.6"]
+      [lein-pprint "1.2.0"]
+      [com.jakemccrary/lein-test-refresh "0.22.0"]
+      [cider/cider-nrepl "0.17.0"]
+      [jonase/eastwood "0.2.6"]
+      [venantius/ultra "0.5.2"]
       [venantius/yagni "0.1.4"]
     ]
     :dependencies [
-      [cljfmt "0.3.0"]
-      [com.cemerick/piggieback "0.2.1"]
-      [org.clojure/tools.nrepl "0.2.12"]
-      [jonase/eastwood "0.2.2"
+      [cljfmt "0.5.7"]
+      [com.cemerick/piggieback "0.2.2"]
+      [org.clojure/tools.nrepl "0.2.13"]
+      [jonase/eastwood "0.2.6"
        :exclusions [org.clojure/clojure]]
     ]
     :repl-options {
