d5e91fafd50d4710c43a75f5e0db2ea98b7259a5
Evan Sosenko <razorx@evansosenko.com>
2018-09-23 10:56:50 -0700

    Update lein profiles

--- dotfiles/lein/profiles.clj
+++ dotfiles/lein/profiles.clj
@@ -3,26 +3,26 @@
     :plugins [
       [lein-ancient "0.6.15"]
       [lein-bikeshed "0.5.1"]
-      [lein-cljfmt "0.5.7"]
+      [lein-cljfmt "0.6.1"]
       [lein-gen "0.2.2"]
       [lein-kibit "0.1.6"]
       [lein-pprint "1.2.0"]
-      [com.jakemccrary/lein-test-refresh "0.22.0"]
-      [cider/cider-nrepl "0.17.0"]
-      [jonase/eastwood "0.2.6"]
+      [com.jakemccrary/lein-test-refresh "0.23.0"]
+      [cider/cider-nrepl "0.18.0"]
+      [jonase/eastwood "0.2.9"]
       [venantius/ultra "0.5.2"]
-      [venantius/yagni "0.1.4"]
+      [venantius/yagni "0.1.6"]
     ]
     :dependencies [
-      [cljfmt "0.5.7"]
-      [com.cemerick/piggieback "0.2.2"]
+      [cljfmt "0.6.1"]
+      [cider/piggieback "0.3.9"]
       [org.clojure/tools.nrepl "0.2.13"]
-      [jonase/eastwood "0.2.6"
+      [jonase/eastwood "0.2.9"
        :exclusions [org.clojure/clojure]]
     ]
     :repl-options {
       :prompt (fn [ns] (str "[" ns "](\u001b[36mλ\u001b[0m)> "))
-      :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]
+      :nrepl-middleware [cider.piggieback/wrap-cljs-repl]
     }
     :generators [
       [lein-gen/generators "0.2.1"]
