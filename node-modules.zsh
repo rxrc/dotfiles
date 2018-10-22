34441a9104e2d35ec4c0319d469ecafe59a91b98
Evan Sosenko <razorx@evansosenko.com>
2018-10-22 09:50:15 -0700

    Update tern

--- node-modules.zsh
+++ node-modules.zsh
@@ -13,7 +13,7 @@ node_modules+=('bower@^1.8.0')
 node_modules+=('gh-home@^1.4.1')
 node_modules+=('jsctags@^5.0.1')
 node_modules+=('json-diff@^0.5.2')
-node_modules+=('tern@^0.21.0')
+node_modules+=('tern@^0.22.3')
 
 npm install --global ${node_modules}
 
