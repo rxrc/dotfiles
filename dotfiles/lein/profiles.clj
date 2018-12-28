a53bed804c5d5d1104a86fad3e1661a4ec01ed81
Evan Sosenko <razorx@evansosenko.com>
2018-12-27 19:18:01 -0600

    Update lein profiles

--- dotfiles/lein/profiles.clj
+++ dotfiles/lein/profiles.clj
@@ -3,22 +3,27 @@
     :plugins [
       [lein-ancient "0.6.15"]
       [lein-bikeshed "0.5.1"]
-      [lein-cljfmt "0.6.1"]
+      [lein-cljfmt "0.6.3"]
       [lein-gen "0.2.2"]
       [lein-kibit "0.1.6"]
       [lein-pprint "1.2.0"]
       [com.jakemccrary/lein-test-refresh "0.23.0"]
       [cider/cider-nrepl "0.18.0"]
-      [jonase/eastwood "0.2.9"]
-      [venantius/ultra "0.5.2"]
-      [venantius/yagni "0.1.6"]
+      [jonase/eastwood "0.3.4"]
+      ; [venantius/ultra "0.5.2"]
+      [venantius/yagni "0.1.7"]
     ]
     :dependencies [
-      [cljfmt "0.6.1"]
-      [cider/piggieback "0.3.9"]
+      [cljfmt "0.6.3"]
+      [cider/piggieback "0.3.10"]
       [org.clojure/tools.nrepl "0.2.13"]
-      [jonase/eastwood "0.2.9"
+      [jonase/eastwood "0.3.4"
        :exclusions [org.clojure/clojure]]
+    ]
+    :middleware [
+      cider-nrepl.plugin/middleware
+      lein-gen.plugin/middleware
+      ; ultra.plugin/middleware
     ]
     :repl-options {
       :prompt (fn [ns] (str "[" ns "](\u001b[36mλ\u001b[0m)> "))
