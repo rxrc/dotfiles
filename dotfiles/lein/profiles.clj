dde356dfdea55e6b74b42f37640604e3074a8e33
Evan Sosenko <razorx@evansosenko.com>
2019-03-14 16:45:57 -0700

    Enable ultra, disable piggieback

--- dotfiles/lein/profiles.clj
+++ dotfiles/lein/profiles.clj
@@ -10,12 +10,12 @@
       [com.jakemccrary/lein-test-refresh "0.23.0"]
       [cider/cider-nrepl "0.21.1"]
       [jonase/eastwood "0.3.5"]
-      ; [venantius/ultra "0.5.2"]
+      [venantius/ultra "0.6.0"]
       [venantius/yagni "0.1.7"]
     ]
     :dependencies [
       [cljfmt "0.6.4"]
-      [cider/piggieback "0.4.0"]
+      ; [cider/piggieback "0.4.0"]
       [org.clojure/tools.nrepl "0.2.13"]
       [jonase/eastwood "0.3.5"
        :exclusions [org.clojure/clojure]]
@@ -23,11 +23,11 @@
     :middleware [
       cider-nrepl.plugin/middleware
       lein-gen.plugin/middleware
-      ; ultra.plugin/middleware
+      ultra.plugin/middleware
     ]
     :repl-options {
       :prompt (fn [ns] (str "[" ns "](\u001b[36mλ\u001b[0m)> "))
-      :nrepl-middleware [cider.piggieback/wrap-cljs-repl]
+      ; :nrepl-middleware [cider.piggieback/wrap-cljs-repl]
     }
     :generators [
       [lein-gen/generators "0.2.1"]
