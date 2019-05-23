0c018c2c3029922c98758c490b381e4b7339ce98
Evan Sosenko <razorx@evansosenko.com>
2019-05-23 11:43:13 -0700

    Update node-modules

--- node-modules.zsh
+++ node-modules.zsh
@@ -10,10 +10,10 @@ command -v npm >/dev/null 2>&1 || exit 0
 npm config set sign-git-tag true
 
 node_modules+=('bower@^1.8.0')
-node_modules+=('gh-home@^1.4.1')
+node_modules+=('gh-home@^2.0.1')
 node_modules+=('jsctags@^5.0.1')
 node_modules+=('json-diff@^0.5.2')
-node_modules+=('tern@^0.22.3')
+node_modules+=('tern@^0.23.0')
 
 npm install --global ${node_modules}
 
