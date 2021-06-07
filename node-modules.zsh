44e43ccc0c3b5751512d27200e1bb9fc59adf963
Evan Sosenko <razorx@evansosenko.com>
2021-06-07 09:59:49 -0700

    REmove gh-home

--- node-modules.zsh
+++ node-modules.zsh
@@ -9,7 +9,6 @@ command -v npm >/dev/null 2>&1 || exit 0
 
 npm config set sign-git-tag true
 
-node_modules+=('gh-home@^3.0.0')
 node_modules+=('jsctags@^5.0.1')
 node_modules+=('json-diff@^0.5.2')
 node_modules+=('tern@^0.24.0')
