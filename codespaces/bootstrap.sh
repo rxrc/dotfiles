f2a44268300fc14b027b14a0fc83ea8611444497
Evan Sosenko <razorx@evansosenko.com>
2022-02-23 11:21:19 -0800

    Update codespaces bootstrap

--- codespaces/bootstrap.sh
+++ codespaces/bootstrap.sh
@@ -3,4 +3,15 @@
 set -e
 set -u
 
+cp -as "$(pwd)/.zshenv" $HOME
 cp -as "$(pwd)/.config/" $HOME
+
+sudo apt-get update -y
+sudo apt-get install -y \
+  ack \
+  httpie \
+  ripgrep \
+  silversearcher-ag
+
+curl -L https://git.io/v5XSs | sh
+ZPLUG_PROTOCOL=HTTPS zsh
