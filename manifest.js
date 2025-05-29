55fb59d48deee81a8700c28ec382da49bce6c7c0
Evan Sosenko <razorx@evansosenko.com>
2025-05-29 11:33:02 -0700

    Switch freyja wallpaper

--- manifest.js
+++ manifest.js
@@ -200,12 +200,12 @@ const symlinks = async () => [{
   hosts: ['mjolnir', 'gungnir'],
   pkgs: ['awesome']
 }, {
-  src: '.config/awesome/themes/powerarrow-dark/wall.png',
+  src: '.config/awesome/themes/powerarrow-dark/wall.png-2',
   dst: await randomFile('drive/wallpaper/hosts/freyja/vert') ?? '.config/awesome/themes/powerarrow/wall.png',
   hosts: ['freyja'],
   pkgs: ['awesome']
 }, {
-  src: '.config/awesome/themes/powerarrow-dark/wall.png-2',
+  src: '.config/awesome/themes/powerarrow-dark/wall.png',
   dst: await randomFile('drive/wallpaper/hosts/freyja') ?? '.config/awesome/themes/powerarrow/wall.png',
   hosts: ['freyja'],
   pkgs: ['awesome']
